defae053e83a9a91e7d85d25a8aa50877735af7b
DESKTOP-NRU09TN\hakjae_chung <jhj13062004@naver.com>
2022-09-14 20:38:29 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 :
    
    *  스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ fintech-arena-be/src/test/java/com/tmaxfintech/fintecharenabe/FintechArenaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmaxfintech.fintecharenabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechArenaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

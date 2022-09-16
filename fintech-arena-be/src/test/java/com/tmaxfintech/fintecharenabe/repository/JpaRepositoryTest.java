f2d0c5174fba48f159cd6b19a095541292443ed5
Hott-J <jhj13062004@naver.com>
2022-09-16 15:59:32 +0900

    #11 - JPA 테스트 작성
    기본 CRUD 와 연관관계 매핑과 cascading 이 잘 동작하는지 보려고 작성함.
    테스트 데이터와 관련이 있기 때문에, 이대로는 실패한다.

---
+++ fintech-arena-be/src/test/java/com/tmaxfintech/fintecharenabe/repository/JpaRepositoryTest.java
@@ -0,0 +1,93 @@
+package com.tmaxfintech.fintecharenabe.repository;
+
+import com.tmaxfintech.fintecharenabe.domain.Article;
+import com.tmaxfintech.fintecharenabe.domain.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("testdb")
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest // 생성자 주입이 아래와 같이 가능해짐
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+        // Given
+
+        // When
+        var articles = articleRepository.findAll();
+
+        // Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(100);
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        var previousCount = articleRepository.count();
+
+        // When
+        var savedArticle = articleRepository.save(Article.of("new article", "new content", "#tmaxfintech"));
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+    }
+
+    @DisplayName("update 테스트")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine() {
+        // Given
+        var article = articleRepository.findById(1L).orElseThrow(); // 없으면 test 종료
+        var updatedHashtag = "#tmaxcommerce";
+        article.setHashtag(updatedHashtag);
+
+        // When
+        var savedArticle = articleRepository.saveAndFlush(article); // flush를 해줘야함. update query발생시킴, 메소드단위로 자동으로 트랜잭셔널 걸려있음 (@DataJpaTest). 기본값 롤백
+
+        // Then
+        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+    }
+
+    @DisplayName("delete 테스트")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine() {
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow(); // 없으면 test 종료
+        var previousArticleCount = articleRepository.count(); // 게시글 수
+        var previousArticleCommentCount = articleCommentRepository.count(); // 댓글 수
+        var deletedCommentsSize = article.getArticleComments().size();
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+        assertThat(articleRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+    }
+}

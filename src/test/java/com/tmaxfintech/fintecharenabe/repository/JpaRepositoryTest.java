b2cc03dcd161ee9d3e1b6c12d406131bcc952745
Hott-J <jhj13062004@naver.com>
2022-09-17 14:34:46 +0900

    #16 - config 폴더 이동에 따른 test 코드 import 수정
    config 폴더를 이동하여 이에 따라 test 코드에서 config 를 의존하는 코드가 수정됨.

--- src/test/java/com/tmaxfintech/fintecharenabe/repository/JpaRepositoryTest.java
+++ src/test/java/com/tmaxfintech/fintecharenabe/repository/JpaRepositoryTest.java
@@ -1,16 +1,13 @@
 package com.tmaxfintech.fintecharenabe.repository;
 
 import com.tmaxfintech.fintecharenabe.domain.Article;
-import com.tmaxfintech.fintecharenabe.domain.config.JpaConfig;
+import com.tmaxfintech.fintecharenabe.config.JpaConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 

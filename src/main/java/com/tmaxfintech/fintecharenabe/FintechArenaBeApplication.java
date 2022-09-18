072d2129e532703540571e74849cbbae26b9d36c
Hott-J <jhj13062004@naver.com>
2022-09-18 16:30:16 +0900

    # 21 - thymeleaf 3 decoupled logic 설정
    java code 로 thymeleaf3 decoupled logic 을 
    사용자 application property 로 제어할 수 있게 추가
    
    * https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

--- src/main/java/com/tmaxfintech/fintecharenabe/FintechArenaBeApplication.java
+++ src/main/java/com/tmaxfintech/fintecharenabe/FintechArenaBeApplication.java
@@ -2,12 +2,12 @@ package com.tmaxfintech.fintecharenabe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FintechArenaBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FintechArenaBeApplication.class, args);
     }
 }
-

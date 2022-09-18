f1f1425a2057b18be855b3e1556dc2efd07af654
Hott-J <jhj13062004@naver.com>
2022-09-18 18:04:51 +0900

    #21 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영
    스프링 시큐리티가 자동 생성해준 `/login` 컨트롤러를 테스트 진행.
    그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@Import` 추가

--- src/test/java/com/tmaxfintech/fintecharenabe/controller/ArticleControllerTest.java
+++ src/test/java/com/tmaxfintech/fintecharenabe/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.tmaxfintech.fintecharenabe.controller;
 
+import com.tmaxfintech.fintecharenabe.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class) // security config 설정 읽음
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -73,5 +76,4 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
-
 }
